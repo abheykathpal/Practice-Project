e2d79cb3cde0e5bf5ac5b2ae2e9c9327db9ec461
abheykathpal <121151442+abheykathpal@users.noreply.github.com>
2024-03-19 01:25:29 +0530

    Done

---
+++ main.js
@@ -0,0 +1,16 @@
+let password = document.querySelector('#inputPassword');
+let button = document.querySelector('button');
+let eyeicon = document.querySelector('#eyeicon')
+
+      button.addEventListener('click', function() {
+        if (password.type === 'password') {
+          password.setAttribute('type', 'text');
+          eyeicon.classList.remove('fa-eye');
+          eyeicon.classList.add('fa-eye-slash');
+        } else {
+          password.setAttribute('type', 'password');
+          eyeicon.classList.remove('fa-eye-slash');
+          eyeicon.classList.add('fa-eye');
+        }
+      });
+    
